83a5527251b0d9faa4405bde98ef7a068d492fe0
Rustam Zavkibekov <r.zavkibekov@gmail.com>
2021-04-05 00:02:51 +0500

    first commit initialization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ruslantito/bank
+
+go 1.16
---
+++ pkg/types/types.go
@@ -0,0 +1,45 @@
+package types
+
+//Money представляют собой денежную сумму в минимальных единицах (центы, копейки, дирамы и тд)
+type Money int64
+
+//Currency представляют код валюты
+type Currency string
+
+//коды валют
+const(
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN представляет номер карты
+type PAN string
+
+//Card представлет информацию о платежной карте
+type Card struct{
+	ID			int
+	PAN			PAN
+	Balance		Money
+	Currency	Currency
+	Color		string
+	Name		string
+	Active		bool
+	MinBalance	Money
+}
+
+//Category представляет собой категорию в который был совершен платеж (авир, аптеки, рестораны и тд)
+type Category string
+
+// Payment представляет информацию о платеже
+type Payment struct{
+	ID int
+	Amount Money
+	Category Category
+}
+
+type PaymentSource struct{
+	Type string //'card'
+	Number string //номер вида '5058 xxxx xxxx 8888'
+	Balance Money //баланс в дирамах
+}
