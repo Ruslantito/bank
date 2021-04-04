9fc54035934c4b18869b722e6208147c866d361d
Rustam Zavkibekov <r.zavkibekov@gmail.com>
2021-04-05 00:14:33 +0500

    chenged types.go and go.mod

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Ruslantito/bank
+module github.com/Ruslantito/bank/v2
 
 go 1.16
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -7,7 +7,7 @@ type Money int64
 type Currency string
 
 //коды валют
-const(
+const (
 	TJS Currency = "TJS"
 	RUB Currency = "RUB"
 	USD Currency = "USD"
@@ -17,29 +17,40 @@ const(
 type PAN string
 
 //Card представлет информацию о платежной карте
-type Card struct{
-	ID			int
-	PAN			PAN
-	Balance		Money
-	Currency	Currency
-	Color		string
-	Name		string
-	Active		bool
-	MinBalance	Money
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
 }
 
 //Category представляет собой категорию в который был совершен платеж (авир, аптеки, рестораны и тд)
 type Category string
 
+//Category представляет собой статус платежа
+type Status string
+
+//
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment представляет информацию о платеже
-type Payment struct{
-	ID int
-	Amount Money
+type Payment struct {
+	ID       int
+	Amount   Money
 	Category Category
+	Status   Status
 }
 
-type PaymentSource struct{
-	Type string //'card'
-	Number string //номер вида '5058 xxxx xxxx 8888'
-	Balance Money //баланс в дирамах
+type PaymentSource struct {
+	Type    string //'card'
+	Number  string //номер вида '5058 xxxx xxxx 8888'
+	Balance Money  //баланс в дирамах
 }
